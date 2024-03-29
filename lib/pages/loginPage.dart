7e177b96aa0d43e75fa482eb3024646d0b21f3d8
Rissip <r.borchani@ecole-ipssi.net>
2024-03-29 10:18:09 +0100

    page login

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginForm extends StatelessWidget {
   final TextEditingController _usernameController = TextEditingController();
@@ -6,6 +9,33 @@ class LoginForm extends StatelessWidget {
   final VoidCallback onLogin;
 
   LoginForm({required this.onLogin});
+
+  Future<void> _login(BuildContext context) async {
+    final username = _usernameController.text;
+    final password = _passwordController.text;
+
+    final response = await http.post(
+      Uri.parse('http://localhost:3000/post/login'),
+      body: jsonEncode({'email': username, 'password': password}),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      final responseData = jsonDecode(response.body);
+      final token = responseData['token'];
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', token);
+
+      onLogin();
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to login. Please try again.'),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,12 +64,7 @@ class LoginForm extends StatelessWidget {
           ),
           const SizedBox(height: 20.0),
           ElevatedButton(
-            onPressed: () {
-              onLogin();
-              //   final username = _usernameController.text;
-              //   final password = _passwordController.text;
-              //   // Implémentez votre logique de connexion ici
-            },
+            onPressed: () => _login(context),
             child: const Text('Login'),
           ),
         ],
